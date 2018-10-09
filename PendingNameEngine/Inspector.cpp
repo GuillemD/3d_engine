ae41f13f2508ebe90cf033a2e951e7c2a3ec4b64
axiermo <axiermopc@gmail.com>
2018-10-09 14:26:30 +0200

    inspector fix

--- PendingNameEngine/Inspector.cpp
+++ PendingNameEngine/Inspector.cpp
@@ -14,9 +14,15 @@ Inspector::~Inspector()
 void Inspector::ShowElement()
 {
 	ImGui::Begin("Inspector");
+	int i = 1;
 	for (std::list<Mesh*>::iterator it = App->scene_intro->scene_objects.begin(); it != App->scene_intro->scene_objects.end(); it++)
 	{
-		ImGui::Text("num vertices: %d", (*it)->data.num_vertex);
+		
+		ImGui::Text("Mesh %d: ", i);
+		ImGui::Text("Total vertices: %d", (*it)->data.num_vertex);
+		ImGui::Text("Total normals: %d", (*it)->data.num_normals);
+		ImGui::Text("Total indices: %d", (*it)->data.num_index);
+		i++;
 	}
 	ImGui::End();
 }
