b912c64e725d87905e4b03a6e89a19b71dfb6516
axiermo <axiermopc@gmail.com>
2018-11-08 17:43:26 +0100

    Component Camera

--- PendingNameEngine/ComponentCamera.cpp
+++ PendingNameEngine/ComponentCamera.cpp
@@ -41,45 +41,62 @@ void ComponentCamera::DrawCameraBoundingBox()
 
 	glBegin(GL_LINES);
 
-	glVertex3f(cameraboundingbox.CornerPoint(0).x, cameraboundingbox.CornerPoint(0).y, cameraboundingbox.CornerPoint(0).z);
-	glVertex3f(cameraboundingbox.CornerPoint(1).x, cameraboundingbox.CornerPoint(1).y, cameraboundingbox.CornerPoint(1).z);
+	{
+		glVertex3f(cameraboundingbox.CornerPoint(0).x, cameraboundingbox.CornerPoint(0).y, cameraboundingbox.CornerPoint(0).z);
+		glVertex3f(cameraboundingbox.CornerPoint(1).x, cameraboundingbox.CornerPoint(1).y, cameraboundingbox.CornerPoint(1).z);
 
-	glVertex3f(cameraboundingbox.CornerPoint(0).x, cameraboundingbox.CornerPoint(0).y, cameraboundingbox.CornerPoint(0).z);
-	glVertex3f(cameraboundingbox.CornerPoint(2).x, cameraboundingbox.CornerPoint(2).y, cameraboundingbox.CornerPoint(2).z);
+		glVertex3f(cameraboundingbox.CornerPoint(0).x, cameraboundingbox.CornerPoint(0).y, cameraboundingbox.CornerPoint(0).z);
+		glVertex3f(cameraboundingbox.CornerPoint(2).x, cameraboundingbox.CornerPoint(2).y, cameraboundingbox.CornerPoint(2).z);
 
-	glVertex3f(cameraboundingbox.CornerPoint(0).x, cameraboundingbox.CornerPoint(0).y, cameraboundingbox.CornerPoint(0).z);
-	glVertex3f(cameraboundingbox.CornerPoint(4).x, cameraboundingbox.CornerPoint(4).y, cameraboundingbox.CornerPoint(4).z);
+		glVertex3f(cameraboundingbox.CornerPoint(0).x, cameraboundingbox.CornerPoint(0).y, cameraboundingbox.CornerPoint(0).z);
+		glVertex3f(cameraboundingbox.CornerPoint(4).x, cameraboundingbox.CornerPoint(4).y, cameraboundingbox.CornerPoint(4).z);
 
-	glVertex3f(cameraboundingbox.CornerPoint(3).x, cameraboundingbox.CornerPoint(3).y, cameraboundingbox.CornerPoint(3).z);
-	glVertex3f(cameraboundingbox.CornerPoint(1).x, cameraboundingbox.CornerPoint(1).y, cameraboundingbox.CornerPoint(1).z);
+		glVertex3f(cameraboundingbox.CornerPoint(3).x, cameraboundingbox.CornerPoint(3).y, cameraboundingbox.CornerPoint(3).z);
+		glVertex3f(cameraboundingbox.CornerPoint(1).x, cameraboundingbox.CornerPoint(1).y, cameraboundingbox.CornerPoint(1).z);
 
-	glVertex3f(cameraboundingbox.CornerPoint(3).x, cameraboundingbox.CornerPoint(3).y, cameraboundingbox.CornerPoint(3).z);
-	glVertex3f(cameraboundingbox.CornerPoint(2).x, cameraboundingbox.CornerPoint(2).y, cameraboundingbox.CornerPoint(2).z);
-													 
-	glVertex3f(cameraboundingbox.CornerPoint(3).x, cameraboundingbox.CornerPoint(3).y, cameraboundingbox.CornerPoint(3).z);
-	glVertex3f(cameraboundingbox.CornerPoint(7).x, cameraboundingbox.CornerPoint(7).y, cameraboundingbox.CornerPoint(7).z);
-													 
-	glVertex3f(cameraboundingbox.CornerPoint(6).x, cameraboundingbox.CornerPoint(6).y, cameraboundingbox.CornerPoint(6).z);
-	glVertex3f(cameraboundingbox.CornerPoint(2).x, cameraboundingbox.CornerPoint(2).y, cameraboundingbox.CornerPoint(2).z);
-													 
-	glVertex3f(cameraboundingbox.CornerPoint(6).x, cameraboundingbox.CornerPoint(6).y, cameraboundingbox.CornerPoint(6).z);
-	glVertex3f(cameraboundingbox.CornerPoint(4).x, cameraboundingbox.CornerPoint(4).y, cameraboundingbox.CornerPoint(4).z);
-												 
-	glVertex3f(cameraboundingbox.CornerPoint(6).x, cameraboundingbox.CornerPoint(6).y, cameraboundingbox.CornerPoint(6).z);
-	glVertex3f(cameraboundingbox.CornerPoint(7).x, cameraboundingbox.CornerPoint(7).y, cameraboundingbox.CornerPoint(7).z);
-													 
-	glVertex3f(cameraboundingbox.CornerPoint(5).x, cameraboundingbox.CornerPoint(5).y, cameraboundingbox.CornerPoint(5).z);
-	glVertex3f(cameraboundingbox.CornerPoint(1).x, cameraboundingbox.CornerPoint(1).y, cameraboundingbox.CornerPoint(1).z);
-													 
-	glVertex3f(cameraboundingbox.CornerPoint(5).x, cameraboundingbox.CornerPoint(5).y, cameraboundingbox.CornerPoint(5).z);
-	glVertex3f(cameraboundingbox.CornerPoint(4).x, cameraboundingbox.CornerPoint(4).y, cameraboundingbox.CornerPoint(4).z);
-													 
-	glVertex3f(cameraboundingbox.CornerPoint(5).x, cameraboundingbox.CornerPoint(5).y, cameraboundingbox.CornerPoint(5).z);
-	glVertex3f(cameraboundingbox.CornerPoint(7).x, cameraboundingbox.CornerPoint(7).y, cameraboundingbox.CornerPoint(7).z);
+		glVertex3f(cameraboundingbox.CornerPoint(3).x, cameraboundingbox.CornerPoint(3).y, cameraboundingbox.CornerPoint(3).z);
+		glVertex3f(cameraboundingbox.CornerPoint(2).x, cameraboundingbox.CornerPoint(2).y, cameraboundingbox.CornerPoint(2).z);
+
+		glVertex3f(cameraboundingbox.CornerPoint(3).x, cameraboundingbox.CornerPoint(3).y, cameraboundingbox.CornerPoint(3).z);
+		glVertex3f(cameraboundingbox.CornerPoint(7).x, cameraboundingbox.CornerPoint(7).y, cameraboundingbox.CornerPoint(7).z);
+
+		glVertex3f(cameraboundingbox.CornerPoint(6).x, cameraboundingbox.CornerPoint(6).y, cameraboundingbox.CornerPoint(6).z);
+		glVertex3f(cameraboundingbox.CornerPoint(2).x, cameraboundingbox.CornerPoint(2).y, cameraboundingbox.CornerPoint(2).z);
+
+		glVertex3f(cameraboundingbox.CornerPoint(6).x, cameraboundingbox.CornerPoint(6).y, cameraboundingbox.CornerPoint(6).z);
+		glVertex3f(cameraboundingbox.CornerPoint(4).x, cameraboundingbox.CornerPoint(4).y, cameraboundingbox.CornerPoint(4).z);
+
+		glVertex3f(cameraboundingbox.CornerPoint(6).x, cameraboundingbox.CornerPoint(6).y, cameraboundingbox.CornerPoint(6).z);
+		glVertex3f(cameraboundingbox.CornerPoint(7).x, cameraboundingbox.CornerPoint(7).y, cameraboundingbox.CornerPoint(7).z);
+
+		glVertex3f(cameraboundingbox.CornerPoint(5).x, cameraboundingbox.CornerPoint(5).y, cameraboundingbox.CornerPoint(5).z);
+		glVertex3f(cameraboundingbox.CornerPoint(1).x, cameraboundingbox.CornerPoint(1).y, cameraboundingbox.CornerPoint(1).z);
+
+		glVertex3f(cameraboundingbox.CornerPoint(5).x, cameraboundingbox.CornerPoint(5).y, cameraboundingbox.CornerPoint(5).z);
+		glVertex3f(cameraboundingbox.CornerPoint(4).x, cameraboundingbox.CornerPoint(4).y, cameraboundingbox.CornerPoint(4).z);
+
+		glVertex3f(cameraboundingbox.CornerPoint(5).x, cameraboundingbox.CornerPoint(5).y, cameraboundingbox.CornerPoint(5).z);
+		glVertex3f(cameraboundingbox.CornerPoint(7).x, cameraboundingbox.CornerPoint(7).y, cameraboundingbox.CornerPoint(7).z);
+	}
 
 	glEnd();
 
 	glColor3f(1, 1, 1);
 	glLineWidth(1.0f);
 
+}
+
+float ComponentCamera::GetNearPlaneDist() const
+{
+	return camerafrustum.nearPlaneDistance;
+}
+
+float ComponentCamera::GetFarPlaneDist() const
+{
+	return camerafrustum.farPlaneDistance;
+}
+
+float ComponentCamera::GetFOV() const
+{
+	return camerafrustum.verticalFov * RADTODEG;
 }
--- PendingNameEngine/ComponentCamera.h
+++ PendingNameEngine/ComponentCamera.h
@@ -5,12 +5,30 @@
 #include "MathGeoLib/MathGeoLib.h"
 #include "Globals.h"
 #include "GLEW/include/glew.h"
+
 class ComponentCamera : public Component {
 public:
 	ComponentCamera();
 	~ComponentCamera();
+
 	void SetAspectRatio(float _aspectratio);
 	void DrawCameraBoundingBox();
+
+
+	float GetNearPlaneDist() const;
+	float GetFarPlaneDist() const;
+	float GetFOV() const;
+	float GetAspectRatio() const;
+
+	void SetNearPlaneDist(float dist);
+	void SetFarPlaneDist(float dist);
+	void SetFOV(float fov);
+	
+	void Look(const float3& position);
+
+	float* GetOpenGLViewMatrix();
+	float* GetOpenGLProjectionMatrix();
+
 public:
 	Frustum camerafrustum;
 	AABB cameraboundingbox;
